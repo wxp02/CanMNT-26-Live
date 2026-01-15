049b72328cabe26beb0e1e77e660cab6ae79bace
wxp02 <wxp0025@gmail.com>
2026-01-14 19:49:17 -0500

    Add adsense tag

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -48,6 +48,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google AdSense */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4504189835164725"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-4H1RW0JYFM"
